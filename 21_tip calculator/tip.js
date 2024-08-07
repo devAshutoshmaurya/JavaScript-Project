ccdf639065bd9c12d9daf4243ed0a9424e2d2a4b
devAshutoshmaurya <ashukum068@gmail.com>
2024-08-08 05:15:21 +0530

    tip calculator

--- 21_tip calculator/tip.js
+++ 21_tip calculator/tip.js
@@ -1,39 +1,71 @@
 let billInput = document.querySelector(".bill-input");
-// console.log(billInput.value)
 let tipBtn = document.querySelectorAll(".btn");
 let customTipValue = document.querySelector(".custom");
 let noOfPeople = document.querySelector(".people-no");
 let tipAmount = document.querySelector("#tip-amount");
 let totalAmount = document.querySelector("#total-amount");
 
-billInput.value = 5.00;
- let bill = parseFloat(billInput);
-console.log(bill)
-// billInput.addEventListener("input", () =>{
-//     let billValue = billInput.value;
-//     console.log(billValue);
-//     calculate();
-// })
 
-// for (const ele of tipBtn) {
-//     console.log(ele.value);
-//     calculate();
-// }
-// noOfPeople.value = 1;
-// noOfPeople.addEventListener("input", () => {
+noOfPeople.value = 1;
+let tipValue = 0;
 
-//     if (noOfPeople.value >= 1) {
-//         calculate();
-//     } else {
+
+
+billInput.addEventListener("input", () => {
+    
+    let calculat = () => {
+        let tip =  ((billInput.value*tipValue)/noOfPeople.value).toFixed(2);
+        // console.log(tip);
+        let x = billInput.value/noOfPeople.value;
+
+        let y = tip/noOfPeople.value;
         
-//     }
+     let total = (x + y).toFixed(2);
+    //  console.log(total);
 
-// })
-// function calculate() {
-//     let tip = (billValue * (ele.value / 100)).toFixed(2);
-//      let tipPerPerson = tip / noOfPeople.value; 
-//      let totalPerPerson = ((tip + tipPerPerson ) / noOfPeople.value).toFixed(2);
-// }
+        tipAmount.innerHTML = `$${tip}`;
+            totalAmount.innerHTML = `$${total}`;
+            
+    }
+    noOfPeople.addEventListener("input", () => {
+       if(noOfPeople.value >= 1){
+        calculat();
+       } 
+      })
+    
+  
+        for (const ele of tipBtn) {
+            // console.dir(ele);
+            ele.addEventListener("click", () => {
+                // ele.classList.add("activeBtn");
+         tipValue =(ele.value/100);
+        calculat();
+          
+            })
+        }   
+        
+        customTipValue.addEventListener("input", () => {  
+             tipValue = (customTipValue.value/100);
+            calculat();
+            
+        })
+
+     calculat();
+       
+});
+
+let resetBtn = document.querySelector(".reset");
+ resetBtn.addEventListener("click", () => {
+    console.log("reset");
+    billInput.value = null;
+    noOfPeople.value = 1;
+    tipAmount.innerHTML = `$0.00`;
+    totalAmount.innerHTML = `$0.00`;
+    // resetBtn.classList.add("activeBtn");
+
+   
+ })
+
 
 
 
